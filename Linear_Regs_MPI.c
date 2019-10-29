a28553f2e0a56eac193c4e42e3e1bf27aa05f80b
Organick <mcagriaksoy@ya.ru>
2019-10-29 22:39:40 +0300

    Add files via upload

--- Linear_Regs_MPI.c
+++ Linear_Regs_MPI.c
@@ -1,23 +1,29 @@
-#include<stdio.h>
+#include "timer.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
-#define S 9900
-char ch, buffer[S];
+
+
+#define SIZE 4000
+int n = 1400;
+char ch, buffer[SIZE];
 int i = 0, j = 0;
-float float_buffer[S];
-float x[S], y[S], sumX=0, sumX2=0, sumY=0, sumXY=0, a, b,local_b;
-int local_result,sumx_local_result=0;
-float x_local[S], y_local[S];
-int source, local_int, n,my_rank, comm_sz, local_n_left = 1, local_n_right,local_n;
-int Regression(int sumX, int sumY, int local_b, int n);
+float float_buffer[SIZE];
+float x[SIZE], y[SIZE], sumX=0, sumX2=0, sumY=0, sumXY=0, a, b,local_b,local_sumX,T_sumXsumY;
+float local_result=0;
+float b_local_result=0;
+int source, local_int,my_rank, comm_sz;
+float  Regression(int sumX, int sumY, int local_b, int n);
 float dummy;
+double start, finish, elapsed;
+void Print_Result(void);
+float f_sumXY(float local_sumX, float local_sumY);
 void Read_File(void)
 {
 // Initializing the file pointer
 FILE *fs;
 fs = fopen("1.txt", "r");
-
 // Read the file unless the file encounters an EOF
 while(1){
 	// Reads the character where the seeker is currently
@@ -27,7 +33,6 @@ while(1){
 	if(ch == EOF){
 		break;
 	}
-
 	// If the delimiter is encounterd(which can be
 	// anything according to your wish) then skip the character
 	// and store the last read array of characters in
@@ -44,17 +49,14 @@ while(1){
 		// arguments, one is a character pointer and 
 		// the other one is the size of the character array
 		bzero(buffer, 32);
-
 		// clearing the counter which counts the number
 		// of character in each number used for reading
 		// into the buffer.
 		i = 0;
-
 		// then continue
 		continue;
 	}
 	else{
-
 		// reads the current character in the buffer
 		buffer[i] = ch;
 		//printf("%c",ch);
@@ -64,17 +66,14 @@ while(1){
 		i++;
 	}
 }
-/* Input */
 	for(i=1;i<=n;i++)
 	{
-	y[i] = float_buffer[i];
+		y[i] = float_buffer[i];
 	}
 }
 
 int main(int argc, char **argv)
 {
-n=500;
-
 /* Let the system do what it needs to start up MPI */
    MPI_Init(NULL, NULL);
 
@@ -88,49 +87,62 @@ n=500;
 Read_File();
 
 
-/* Calculating Required Sum */
+//local_sumY = sumY/comm_sz;
 
-for(i=1;i<=local_n;i++)
+
+/* Calculating Required Sum */
+GET_TIME(start);
+for(i=1;i<=n;i++)
 {
 sumX = sumX + x[i];
 sumY = sumY + y[i];
 sumX2 = sumX2 + x[i]*x[i];
 sumXY = sumXY + x[i]*y[i];
 }
-dummy = sumY /n ;
-b = (n*sumXY-sumX*sumY)/(n*sumX2-sumX*sumX);
-local_b = b/comm_sz;
+local_sumX = sumX/comm_sz;
+float local_sumXsumY = f_sumXY(local_sumX,sumY);
 
+MPI_Reduce(&local_sumXsumY, & T_sumXsumY, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
+b = (n*sumXY-T_sumXsumY)/(n*sumX2-sumX*sumX);
+local_b = b/comm_sz;
 local_result = Regression(sumX, sumY,local_b,n);
  /* Add up the integrals calculated by each process */
-   MPI_Reduce(&local_result, & sumx_local_result, 1, MPI_INT, MPI_SUM, 0,
-         MPI_COMM_WORLD);
+MPI_Reduce(&local_result, & b_local_result, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-printf("A::%d", &sumx_local_result);
-a = dummy - sumx_local_result;
+ if (my_rank == 0) {
+     	Print_Result();
+	 GET_TIME(finish);
+	 elapsed = finish - start;
+	printf("The code to be timed took %e seconds\n", elapsed);
+   }
+ /* Shut down MPI */
+   MPI_Finalize();
+return(0);
+}
+float f_sumXY(float local_sumX,float local_sumY)
+{
+	return (local_sumX*local_sumY);
+}
+float Regression(int sumX, int sumY, int local_b, int n)
+{
+	return (local_b*sumX)/n;
+}
+
+void Print_Result(void){
+
+a = (sumY /n) - b_local_result;
 /* Displaying value of a and b */
 printf("Values are: a=%0.2f and b = %0.2f",a,b);
 printf("\nEquation of best fit is: y = %0.2f + %0.2fx",a,b);
 float res = a + (b * (n+1));
 if(res < y[n] )
 {
-printf("NEXT DAY, THE STOCK PRICE WILL BE: DECREASED!!\n");
+printf("\n NEXT DAY, THE STOCK PRICE WILL BE: DECREASED!!\n");
 }
 else{
-printf("NEXT DAY, THE STOCK PRICE WILL BE: INCREASED!!\n");
+printf("\n NEXT DAY, THE STOCK PRICE WILL BE: INCREASED!!\n");
 }
-printf("Next Stock Price Prediction is: %0.2f " , res);
-
- 
-   /* Shut down MPI */
-   MPI_Finalize();
-
-return(0);
-}
-
-int Regression(int sumX, int sumY, int local_b, int n)
-{
-a = (local_b*sumX)/n;
-
-return a;
+printf("\n Next Stock Price Prediction is: %0.2f " , res);
 }
