71198d17bd2ec32108234e8b340ff17fa0180c8f
Organick <mcagriaksoy@ya.ru>
2019-11-04 01:02:44 +0300

    Add files via upload

--- Linear_Regs_PThread.c
+++ Linear_Regs_PThread.c
@@ -4,7 +4,7 @@
  * 
  * License:  GNU General Public License v3.0
  * 
- * Purpose:  A program in which multiple MPI processes try to predict Stock Prices in the next day.
+ * Purpose:  A program in which multiple PThread processes try to predict Stock Prices in the next day.
  * 
  * Compile:
  *    gcc -g -Wall -o  Linear_Regs_PThread Linear_Regs_PThread.c -lpthread
@@ -21,19 +21,17 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include "timer.h"
-
-#define S 9900
+#define S 2000
+/* Global variables */
 int n,m;
-// Initializing the file pointer
 const int MAX_THREADS = 128;
 int arr[128];
-char ch, buffer[9900];
+char ch, buffer[S];
 int i = 0, j = 0;
-float float_buffer[9900];
+float float_buffer[S];
 float x[S], y[S], sumX=0, sumX2=0, sumY=0, sumXY=0, a, b;
-/* Global variables */
 int     thread_count;
-  double start, finish;
+double start, finish;
 
 /* Parallel function */
 void *Pth_mat_vect(void* rank);
@@ -41,111 +39,98 @@ void Usage(char* prog_name);
 
 int main(int argc, char **argv)
 {
+	long       thread;
+	pthread_t* thread_handles;
+	FILE *fs;
+	fs = fopen("TurkishAirlinesDATASET.csv", "r");
+	// Read the file unless the file encounters an EOF
+	while(1){
+		// Reads the character where the seeker is currently
+		ch = fgetc(fs);
 
-long       thread;
-pthread_t* thread_handles;
-FILE *fs;
-fs = fopen("1.csv", "r");
-// Read the file unless the file encounters an EOF
-while(1){
-	// Reads the character where the seeker is currently
-	ch = fgetc(fs);
+		// If EOF is encountered then break out of the while loop
+		if(ch == EOF){
+			break;
+		}
 
-	// If EOF is encountered then break out of the while loop
-	if(ch == EOF){
-		break;
+		// If the delimiter is encounterd(which can be
+		// anything according to your wish) then skip the character
+		// and store the last read array of characters in
+		// an integer array
+		else if(ch == '\n'){
+	// Increamenting the array position
+			j++;
+			// Converting the content of the buffer into
+			// an array position
+			y[j] = atoi(buffer);
+			//printf("%0.2f",float_buffer[j]);		
+			x[j]=x[j-1]+1;
+			// Clearing the buffer, this function takes two
+			// arguments, one is a character pointer and 
+			// the other one is the size of the character array
+			bzero(buffer, 32);
+
+			// clearing the counter which counts the number
+			// of character in each number used for reading
+			// into the buffer.
+			i = 0;
+
+			// then continue
+			continue;
+		}
+		else{
+
+			// reads the current character in the buffer
+			buffer[i] = ch;
+			//printf("%c",ch);
+			// increamenting the counter so that the next
+			// character is read in the next position in 
+			// the array of buffer
+			i++;
+		}
 	}
+	/* Input */
+	printf("How many lines of Stock Price will be covered? (Higher numbers checks higher number of stocks Max:1503 )\n");
+	scanf("%d", &n);
 
-	// If the delimiter is encounterd(which can be
-	// anything according to your wish) then skip the character
-	// and store the last read array of characters in
-	// an integer array
-	else if(ch == '\n'){
-// Increamenting the array position
-		j++;
-		// Converting the content of the buffer into
-		// an array position
-		y[j] = atoi(buffer);
-		//printf("%0.2f",float_buffer[j]);		
-		x[j]=x[j-1]+1;
-		// Clearing the buffer, this function takes two
-		// arguments, one is a character pointer and 
-		// the other one is the size of the character array
-		bzero(buffer, 32);
+	   /* Get number of threads from command line */
+	   if (argc != 2) Usage(argv[0]);
+	   thread_count = strtol(argv[1], NULL, 10);  
+	   if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
 
-		// clearing the counter which counts the number
-		// of character in each number used for reading
-		// into the buffer.
-		i = 0;
+	   thread_handles = malloc (thread_count*sizeof(pthread_t)); 
 
-		// then continue
-		continue;
+	   for (thread = 0; thread < thread_count; thread++)
+	      pthread_create(&thread_handles[thread], NULL,
+		 Pth_mat_vect, (void*) thread);
+	   for (thread = 0; thread < thread_count; thread++)
+	      pthread_join(thread_handles[thread], NULL);
+
+	b = (n*sumXY-sumX*sumY)/(n*sumX2-sumX*sumX);
+	a = (sumY - b*sumX)/n;	
+	//printf("Values are: a=%0.2f and b = %0.2f",a,b);
+	printf("\nEquation of best fit is: y = %0.2f + %0.2fx",a,b);
+	float res = a + (b * (n+1));
+	if(res < y[n] )
+	{
+		printf("NEXT DAY, THE STOCK PRICE WILL BE: DECREASED!!\n");
 	}
 	else{
-
-		// reads the current character in the buffer
-		buffer[i] = ch;
-		//printf("%c",ch);
-		// increamenting the counter so that the next
-		// character is read in the next position in 
-		// the array of buffer
-		i++;
+		printf("NEXT DAY, THE STOCK PRICE WILL BE: INCREASED!!\n");
+		}
+		printf("Next Stock Price Prediction is: %0.2f " , res);
+	int resultime =0;
+	for (int i = 0; i <= thread; i++){
+		resultime += arr[i];
 	}
+	 printf("Thread > Elapsed time = %e seconds\n", finish - start);
+	 free(thread_handles);
+	return(0);
 }
-/* Input */
-printf("How many lines of Stock Price will be covered? (Higher numbers checks higher number of stocks Max:1530 )\n");
-scanf("%d", &n);
-
-   /* Get number of threads from command line */
-   if (argc != 2) Usage(argv[0]);
-   thread_count = strtol(argv[1], NULL, 10);  
-   if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
-
-   thread_handles = malloc (thread_count*sizeof(pthread_t)); 
-
-   for (thread = 0; thread < thread_count; thread++)
-      pthread_create(&thread_handles[thread], NULL,
-         Pth_mat_vect, (void*) thread);
- printf("Hello from the main thread\n");
-
-   for (thread = 0; thread < thread_count; thread++)
-      pthread_join(thread_handles[thread], NULL);
-/* Calculating Required Sum 
-for(i=1;i<=n;i++)
-{
-sumX = sumX + x[i];
-sumX2 = sumX2 + x[i]*x[i];
-sumY = sumY + y[i];
-sumXY = sumXY + x[i]*y[i];
-}*/
-/* Calculating a and b */
-/* Displaying value of a and b */
-b = (n*sumXY-sumX*sumY)/(n*sumX2-sumX*sumX);
-a = (sumY - b*sumX)/n;	
-//printf("Values are: a=%0.2f and b = %0.2f",a,b);
-printf("\nEquation of best fit is: y = %0.2f + %0.2fx",a,b);
-float res = a + (b * (n+1));
-if(res < y[n] )
-{
-printf("NEXT DAY, THE STOCK PRICE WILL BE: DECREASED!!\n");
-}
-else{
-printf("NEXT DAY, THE STOCK PRICE WILL BE: INCREASED!!\n");
-}
-printf("Next Stock Price Prediction is: %0.2f " , res);
-int resultime =0;
-for (int i = 0; i <= thread; i++){
-resultime += arr[i];
-}
- printf("Thread > Elapsed time = %e seconds\n", finish - start);
- free(thread_handles);
-return(0);
-}
-
 
 /*------------------------------------------------------------------
  * Function:       Pth_mat_vect
- * Purpose:        Multiply an mxn matrix by an nx1 column vector
+ * Purpose:        Calculate result of regression to create formula.
  * In arg:         rank
  * Global in vars: A, x, m, n, thread_count
  * Global out var: y
@@ -167,7 +152,7 @@ void *Pth_mat_vect(void* rank) {
    GET_TIME(finish);
   arr[my_rank] = finish - start;
    return NULL;
-}  /* Pth_mat_vect */
+} 
 
 /*-------------------------------------------------------------------*/
 void Usage(char* prog_name) {
